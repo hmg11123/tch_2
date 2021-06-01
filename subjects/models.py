e6878f13e7d0dd902a362b713304e77363cccf62
hmg11123 <you@example.com>
2021-06-01 21:01:01 +0900

    subject difficulty

--- subjects/models.py
+++ subjects/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 class Subject(models.Model):
 
+    CHOICE_EASY = "easy"
+    CHOICE_USUALLY = "usually"
+    CHOICE_DIFFICULT = "difficult"
+
+    CHOICE_DIFFICULTY = (
+        (CHOICE_EASY, "EASY"),
+        (CHOICE_USUALLY, "USUALLY"),
+        (CHOICE_DIFFICULT, "DIFFICULT"),
+    )
+
     name = models.CharField(max_length=100)
+    subject_difficulty = models.CharField(choices=CHOICE_DIFFICULTY, max_length=40)
     
     class Meta:
         verbose_name_plural = "과목 관리"
